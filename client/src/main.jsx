445bd147610faa6d61c8577850a71a36742bf101
KATHIRVEL133 <velkathir259@gmail.com>
2024-09-30 18:12:11 +0530

    complete dark mode functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,14 @@ import {persistor,store} from './redux/store.js'
 import {Provider} from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import './index.css'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
    <PersistGate loading={null} persistor={persistor}>
+    <ThemeProvider>
      <App />
+    </ThemeProvider>
    </PersistGate>
   </Provider>
   
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
 import {combineReducers,configureStore} from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import themeReducer from './theme/themeSlice'
 import {persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import persistStore from 'redux-persist/es/persistStore'
-const rootReducer = combineReducers({user:userReducer});
+const rootReducer = combineReducers(
+    {
+        user:userReducer,
+        theme:themeReducer,
+    }
+);
 const persistConfig = {
     key:'root',
     storage,
