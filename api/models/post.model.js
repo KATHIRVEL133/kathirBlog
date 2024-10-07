154d6d64d43c8d28053fa55dedd567dc04668b94
KATHIRVEL133 <velkathir259@gmail.com>
2024-10-07 13:21:31 +0530

    completed image upload functionality in create post

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -15,8 +15,8 @@ const postSchema = new mongoose.Schema(
             unique:true,
         },
         image:{
-            type:String,
-            default:'https://www.blogtyrant.com/wp-content/uploads/2017/02/how-to-write-a-good-blog-post.png',
+            type:Array,
+            default:['https://www.blogtyrant.com/wp-content/uploads/2017/02/how-to-write-a-good-blog-post.png'],
         },
         category:
         {
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,70 @@
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
+/* eslint-disable no-unused-vars */
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
+import { useEffect, useState } from "react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+
 
 export default function CreatePost() {
+  const [imageFile,setImageFile] = useState([]);
+  const [imageFileUrl,setImageFileUrl] = useState([]);
+  const [imageUploadError,setImageUploadError] = useState(null);
+  const [imageUploadProgress,setImageUplaodProgress] = useState(0);
+  console.log(imageFileUrl);
+  const handleImageFileChange = (e)=>
+  {
+
+   for(let i=0;i<e.target.files.length;i++)
+   {
+     const file = e.target.files[i];
+     if(file)
+     {
+      
+      setImageFile((prevImageFile)=> [...prevImageFile,file]);
+    
+     }
+   }
+  }
+ 
+  const handleUploadImage = ()=>
+  {
+    if(imageFile.length==0)
+    {
+      setImageUploadError('Please select atleast one file');
+    }
+    for(let i=0;i<imageFile.length;i++)
+    {
+     uploadImage(i);
+    }
+  }
+  const uploadImage = async (i)=>
+  {
+  setImageUploadError(null);
+  const storage = getStorage(app);
+  const fileName = new Date().getTime() + imageFile[i].name;
+  const storageRef = ref(storage,fileName);
+  const uploadTask = uploadBytesResumable(storageRef,imageFile[i]);
+  uploadTask.on('state_changed',(snapshot)=>
+  {
+  const progress = (snapshot.bytesTransferred/snapshot.totalBytes);
+  setImageUplaodProgress(progress.toFixed(0));
+  },(error)=>
+  {
+    setImageUploadError('Image must be less than (2mb) size');
+    setImageUplaodProgress(0);
+    setImageFile(null);
+    setImageFileUrl(null);
+  },()=>
+  {
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl)=>
+    {
+      setImageFileUrl((prevImageFileUrl)=> [...prevImageFileUrl,downloadUrl]);
+    })
+  });
+  }
   return (
     <div className="min-h-screen max-w-3xl mx-auto p-3">
       <h1 className="text-center text-3xl font-semibold my-7">
@@ -29,9 +91,26 @@ export default function CreatePost() {
        </Select>
         </div>
         <div className="flex p-3 border-4 gap-4 justify-between border-teal-500 border-dotted">
-         <FileInput type='file' accept="image/*"/>
-         <Button type="button" gradientDuoTone="purpleToBlue" size='sm' outline>Upload Image</Button>
+         <FileInput type='file' accept="image/*" onChange={handleImageFileChange} multiple/>
+         <Button type="button" gradientDuoTone="purpleToBlue" size='sm' outline onClick={handleUploadImage}>
+          Upload Image
+          </Button>
+      
         </div>
+        <div className="grid grid-cols-5 max-w-3xl overflow-hidden gap-4 "> 
+         {
+         imageFileUrl&&imageFileUrl.map((urls,index)=>
+        <img key={index} src={urls} className="flex-1 w-full h-full object-cover shadow-lg">
+         
+        </img>
+        )
+         }
+        </div>
+        {
+          imageUploadError&&<Alert color='failure'>
+            {imageUploadError}
+          </Alert>
+        }
         <ReactQuill theme="snow" placeholder="write something..." className="h-72 mb-12" id="content" required/>
         <Button type="submit" gradientDuoTone='purpleToPink'>
             Publish
