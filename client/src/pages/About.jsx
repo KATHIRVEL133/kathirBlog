33f3118acde8f496f61933dc298d8cc7263ca51b
KATHIRVEL133 <velkathir259@gmail.com>
2024-10-28 14:23:08 +0530

    Completed the project

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,15 @@
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className="max-w-6xl min-h-screen p-4"> 
+      <div className="border-2 border-gray-400 p-3 w-[800px] h-[400px] mx-auto shadow-lg my-14">
+      <h1 className="font-bold text-center mb-3 text-[20px] ">
+        About the project development
+      </h1>
+      <p className="text-center">
+        I have done this project by following the youtube channel. By completing this project I had gained lot of information about development.
+      </p>
+      </div>
     </div>
   )
 }
